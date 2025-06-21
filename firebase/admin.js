5457bf3a563d41c7ffc626f28ab08f507da48604
Wellingtondela <wsatsg@gmail.com>
2025-06-21 13:52:44 -0300

    hvhgv

--- firebase/admin.js
+++ firebase/admin.js
@@ -5,6 +5,8 @@ dotenv.config();
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://wsapet-ff5fb.firebaseio.com"
